bb0f1792b0fe2783d43c336b0f5394b4a8a571f7
basterdator <34163140+basterdator@users.noreply.github.com>
2017-12-01 17:46:35 +0200

    Commit

---
+++ ex2/ex2/main.c
@@ -0,0 +1,15 @@
+// ==============================================
+// Introduction to Systems Programming
+//			 Winter 2017-2018
+//			TEL-AVIV UNIVERSITY
+//			     Exercise 1
+// Uri Cohen                 302825807
+// Anton Chaplianka          310224209
+//												®
+// ==============================================
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
