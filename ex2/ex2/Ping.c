606b98d8b3ab57092ad9dd380f7ad1430fad28f6
basterdator <34163140+basterdator@users.noreply.github.com>
2017-12-06 01:18:14 +0200

    !

--- ex2/ex2/Ping.c
+++ ex2/ex2/Ping.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <windows.h>
-#include "CreateThreadSimple.h"
 
-void main() 
+
+DWORD WINAPI Ping(LPVOID lpParam)
 {
-
+	return 0;
 }
---
+++ ex2/ex2/Ping.h
@@ -0,0 +1,12 @@
+#ifndef __PING_H__
+#define __PING_H__
+// Includes --------------------------------------------------------------------
+
+#include <windows.h>
+
+// Function Declarations -------------------------------------------------------
+
+
+DWORD WINAPI Ping(LPVOID lpParam);
+
+#endif // __PING_H__
